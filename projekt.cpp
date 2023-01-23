0060a9a07a1bee9d8466ed63352f5a583f5253ed
mk306207 <kolbermateusz@gmail.com>
2023-01-23 22:16:38 +0100

    init

--- projekt.cpp
+++ projekt.cpp
@@ -1,5 +1,38 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+#include <vector>
+#include <sstream>
 
-int main(){
-    std::cout<<"chuj w dupe oziemu";
+struct Food {
+    int id;
+    std::string nazwa;
+    std::string kategoria;
+    float cena;
+    std::string opis;
+};
+//struktura
+int main() {
+    std::ifstream inFile;
+    inFile.open("baza.txt");
+    std::string line;
+    std::map<std::string, std::vector<Food>> menu;
+    std::vector<Food> zamowienie;
+    int id;
+    std::string nazwa, kategoria;
+    float cena;
+    std::string opis;
+        while(std::getline(inFile, line))
+        {
+            std::stringstream ss(line);//wybieranie liczb ze stringa
+            ss >> id; // zmienna ss do id
+            getline(ss, nazwa, ';');//czytam linie dopoki nie natrafie na ;
+            getline(ss, kategoria, ';');
+            ss >> cena;  // zmienna ss do ceny
+            getline(ss, opis);
+            Food f = {id, nazwa, kategoria, cena, opis};
+            menu[kategoria].push_back(f);
+        }
+    inFile.close();
 }
